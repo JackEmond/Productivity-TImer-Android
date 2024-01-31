580b41500b657142666ed301d4d299aa4dc185c0
Jack Emond <JackEmond@outlook.com>
2024-01-31 14:17:54 -0500

    Changed Default Colour in Theme

--- app/src/main/java/com/example/productivitytimer/ui/theme/Type.kt
+++ app/src/main/java/com/example/productivitytimer/ui/theme/Type.kt
@@ -2,14 +2,19 @@ package com.example.productivitytimer.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.productivitytimer.R
 
+val CustomFontFamily = FontFamily(
+    Font(R.font.bebas_neue) // Replace 'your_font_file' with your actual font file name
+)
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = CustomFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
