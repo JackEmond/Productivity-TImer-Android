0a2e8cb937038f911bfc0eb7a9e47980ae066628
Jack Emond <JackEmond@outlook.com>
2024-02-08 16:48:06 -0500

    Modified timer page so it displays the timer incrementing on start

--- app/src/main/java/com/example/productivitytimer/ui/TimerPage.kt
+++ app/src/main/java/com/example/productivitytimer/ui/TimerPage.kt
@@ -12,6 +12,7 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -20,15 +21,23 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.example.productivitytimer.ui.theme.ProductivityTimerTheme
+import com.example.productivitytimer.viewmodel.TimerViewModel
 
 
 @Composable
 fun TimerPage(
     navController: NavHostController,
+    timerViewModel: TimerViewModel = viewModel()
 ){
+    //When app starts, start the timer
+    LaunchedEffect(key1 = true) {
+        timerViewModel.startTimer()
+    }
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -36,45 +45,55 @@ fun TimerPage(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ){
-        ProductivityTimerText()
-        TimerText()
-        Buttons{navController.navigate("CreateTimerPage")}
+        HeaderText()
+        TimerText(timerViewModel.time)
+        Buttons(navController, timerViewModel)
     }
 }
 
-@Composable
-private fun Buttons(navigate: () -> Unit) {
-    Row(){
-        TimerButton(text = "Pause", onClick = { /*TODO*/ })
-        TimerButton(text = "Cancel", onClick = navigate)
-        TimerButton(text = "Save", onClick = { /*TODO*/ })
-    }
 
+@Composable
+private fun Buttons(
+    navController: NavHostController, timerViewModel: TimerViewModel,
+) {
+    Row(){
+        TimerButton(text = "Pause", onClick = { timerViewModel.pauseTimer()})
+
+        TimerButton(
+            text = "Cancel",
+            onClick = {
+                timerViewModel.cancelTimer()
+                navController.navigate("CreateTimerPage")
+            }
+        )
+
+        TimerButton(text = "Save", onClick = { timerViewModel.saveTimer()})
+    }
 }
 
 @Composable
 private fun TimerButton(
     text: String,
     onClick: () -> Unit,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+) {
+    Button(
+        onClick = onClick,
+        modifier = modifier.padding(10.dp),
+        colors = ButtonDefaults.buttonColors(Color.Black),
+        shape = RectangleShape,
     ) {
-        Button(
-            onClick = onClick,
-            modifier = modifier.padding(10.dp),
-            colors = ButtonDefaults.buttonColors(Color.Black),
-            shape = RectangleShape,
-            ) {
-            Text(
-                text = text.uppercase(),
-                Modifier.padding(5.dp),
-                fontSize = 15.sp
-            )
-        }
+        Text(
+            text = text.uppercase(),
+            Modifier.padding(5.dp),
+            fontSize = 15.sp
+        )
+    }
 }
 
 
 @Composable
-private fun ProductivityTimerText() {
+private fun HeaderText() {
     Text(
         text = "Productivity Timer",
         fontSize = 50.sp,
@@ -83,7 +102,9 @@ private fun ProductivityTimerText() {
 }
 
 @Composable
-private fun TimerText() {
+private fun TimerText(time: Int) {
+    val formattedTime = formatTime(time)
+
     Text(
         modifier = Modifier
             .fillMaxWidth()
@@ -91,10 +112,18 @@ private fun TimerText() {
             .padding(35.dp)
         ,
         fontSize = 40.sp,
-        text = "00hHRS 00MIN 00SEC",
+        text = formattedTime,
         color = Color.White,
         textAlign = TextAlign.Center,
     )
+}
+
+fun formatTime(time: Int): String {
+    val hours = time / 3600
+    val minutes = (time % 3600) / 60
+    val seconds = time % 60
+
+    return String.format("%02dHRS %02dMIN %02dSEC", hours, minutes, seconds)
 }
 
 @Preview(showBackground = true)
