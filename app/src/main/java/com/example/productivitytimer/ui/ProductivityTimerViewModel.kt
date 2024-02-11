4e9020fa0b210bf3e9c459b2b2a7a865dde6dbc1
Jack Emond <JackEmond@outlook.com>
2024-02-11 15:23:16 -0500

    moved ProductivityTimerViewModel to UI folder

---
+++ app/src/main/java/com/example/productivitytimer/ui/ProductivityTimerViewModel.kt
@@ -0,0 +1,69 @@
+package com.example.productivitytimer.ui
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.productivitytimer.data.ProductivityTimerDBRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class ProductivityTimerViewModel @Inject constructor(
+    private val repository: ProductivityTimerDBRepository
+
+): ViewModel() {
+    private val scope = viewModelScope
+    private var job: Job? = null
+    var time by mutableIntStateOf(0) // Time in seconds
+    private var isActive = false
+
+
+    fun startTimer() {
+        startIncrementingTime()
+        addTimerToDatabase()
+    }
+
+    private fun addTimerToDatabase()
+            = viewModelScope.launch{
+        repository.insertTime(time)
+    }
+
+
+    private fun startIncrementingTime(){
+        job = scope.launch {
+            isActive = true
+            while (isActive) {
+                time++
+                delay(1000L) // Wait for a second
+            }
+        }
+    }
+
+
+    fun pauseTimer(){
+        job?.cancel()
+    }
+
+    fun saveTimer(){
+        //TODO:
+    }
+
+    fun cancelTimer() {
+        //Timer.cancel();
+        job?.cancel()
+        //stopTimerService()
+    }
+
+
+
+
+
+
+
+
+}
