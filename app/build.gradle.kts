4176997b28f2443e2b489587c6631bd3a2228628
Jack Emond <JackEmond@outlook.com>
2024-02-11 15:25:04 -0500

    added androidx.hilt:hilt-navigation-compose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,6 +72,7 @@ dependencies {
 
     implementation("com.google.dagger:hilt-android:2.50")
     kapt("com.google.dagger:hilt-android-compiler:2.50")
+    implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
 
     implementation("androidx.room:room-common:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
