914c7d540f4e97c03a8f9c310660d017cf1f16b4
Jack Emond <JackEmond@outlook.com>
2024-02-16 15:43:27 -0500

    changed pause button onclick naming convention

--- app/src/main/java/com/example/productivitytimer/ui/ProductivityTimerViewModel.kt
+++ app/src/main/java/com/example/productivitytimer/ui/ProductivityTimerViewModel.kt
@@ -1,16 +1,11 @@
 package com.example.productivitytimer.ui
 
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.map
 import androidx.lifecycle.viewModelScope
 import com.example.productivitytimer.data.ProductivityTimerDBRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -35,7 +30,7 @@ class ProductivityTimerViewModel @Inject constructor(
         timer.start()
     }
 
-    fun pauseTimerButtonClicked() {
+    fun pauseOrResumeTimer() {
         timer.pauseOrResume()
     }
 
--- app/src/main/java/com/example/productivitytimer/ui/TimerPage.kt
+++ app/src/main/java/com/example/productivitytimer/ui/TimerPage.kt
@@ -68,7 +68,7 @@ private fun Buttons(
         TimerButton(
             text = if (isPaused) "Resume" else "Pause",
             onClick = {
-                timerVM.pauseTimerButtonClicked()
+                timerVM.pauseOrResumeTimer()
             }
         )
 
