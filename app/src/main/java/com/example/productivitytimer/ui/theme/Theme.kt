f57cc95ff6785505d76bb1f712115aa959328110
Jack Emond <JackEmond@outlook.com>
2024-03-07 13:00:49 -0500

    stop using dynamicColour

--- app/src/main/java/com/example/productivitytimer/ui/theme/Theme.kt
+++ app/src/main/java/com/example/productivitytimer/ui/theme/Theme.kt
@@ -20,35 +20,23 @@ private val DarkColorScheme = darkColorScheme(
     primary = Color.White,
     secondary = Color.Black,
     tertiary = Color.Yellow,
-   //background = Color(0xFFFFFBFE),
-   //surface = Color(0xFFFFFBFE),
    onPrimary = Color.Black,
    onSecondary = Color.Green,
    onTertiary = Color.Green,
-   //onBackground = Color(0xFFFFFFFF),
-   //onSurface = Color(0xFF1C1B1F),
 )
 
 private val LightColorScheme = lightColorScheme(
     primary = Color.Black,
     secondary = Color.White,
     tertiary = Color.Blue
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+
 )
 
 @Composable
 fun ProductivityTimerTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
