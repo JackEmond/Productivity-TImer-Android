c1d66805edb4b16f4b02e44097e2773bb882d5c8
Jack Emond <JackEmond@outlook.com>
2024-04-12 20:38:07 -0400

    added getTimeProductiveEachDay query to DAO

--- app/src/main/java/com/example/productivitytimer/data/local/TimerRecordDao.kt
+++ app/src/main/java/com/example/productivitytimer/data/local/TimerRecordDao.kt
@@ -15,10 +15,13 @@ interface TimerRecordDao {
     @Query("Select * FROM timerrecord ORDER BY DATE DESC")
     fun getAllTimers(): LiveData<List<TimerRecord>>
 
-    @Query("SELECT SUM(time) as sumTime, strftime('%w', date / 1000, 'unixepoch') as dayOfWeek FROM timerrecord WHERE date > :sevenDaysAgo GROUP BY dayOfWeek")
+    @Query("SELECT SUM(time) as sumTime, strftime('%w', date / 1000, 'unixepoch') as day FROM timerrecord WHERE date > :sevenDaysAgo GROUP BY day")
     fun getTimersFromLast7Days(sevenDaysAgo: Long): Flow<List<TimeRanEachDay>>
 
-    data class TimeRanEachDay(val dayOfWeek: String, val sumTime: Int)
+    @Query("SELECT date(date/1000, 'unixepoch') as day, SUM(time) as sumTime FROM timerrecord WHERE date >= :earlierDate AND date <= :laterDate GROUP BY day ORDER BY day DESC")
+    fun getTimeProductiveEachDay(earlierDate: Long, laterDate: Long): Flow<List<TimeRanEachDay>>
+
+    data class TimeRanEachDay(val day: String, val sumTime: Int)
 
     @Query("DELETE FROM timerrecord WHERE id = :id")
     suspend fun deleteById(id: Int)
