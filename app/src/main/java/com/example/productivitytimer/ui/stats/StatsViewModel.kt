c500f4cff485dcd22bf6f2950a32b1414f5ad649
Jack Emond <JackEmond@outlook.com>
2024-04-23 16:23:20 -0400

    moved TimerRecord outside StatsViewModel and into its own data class

--- app/src/main/java/com/example/productivitytimer/ui/stats/StatsViewModel.kt
+++ app/src/main/java/com/example/productivitytimer/ui/stats/StatsViewModel.kt
@@ -16,25 +16,6 @@ import java.util.Calendar
 import java.util.Locale
 import javax.inject.Inject
 
-
-data class TimerRecord(
-    val id: Int = 0,
-    var date: String = "",
-    var timeInSeconds: Int = 0
-) {
-}
-
-fun TimerRecord.getFormattedTime(): String {
-    val hours = timeInSeconds / 3600
-    val minutes = (timeInSeconds % 3600) / 60
-    val seconds = timeInSeconds % 60
-    return when {
-        hours > 0 -> String.format("%02d HRS %02d MIN %02d SEC", hours, minutes, seconds)
-        minutes > 0 -> String.format("%02d MIN %02d SEC", minutes, seconds)
-        else -> String.format("%02d SEC", seconds)
-    }
-}
-
 @HiltViewModel
 class StatsViewModel @Inject constructor(
     private val repository: ProductivityTimerDBRepository,
